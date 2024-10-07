61116755b5f2df89cb1ef6915667220c89840b15
ArsenalRegistry <132525420+ArsenalRegistry@users.noreply.github.com>
2024-10-07 16:39:07 +0900

    app.vue 수정완료2로 변경

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>수정완료</div>
+    <div>수정완료2</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
